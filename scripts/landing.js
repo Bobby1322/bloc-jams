1aa8905d8256d8ce548a707cb3bc0bd047285e7b
Bob-e <bob.e.mercer@gmail.com>
2017-06-22 18:47:59 -0400

    Checkpoint 9 Dom Event: Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-        var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+
 
         var revealPoints = function(index) {
             points[index].style.opacity = 1;
@@ -21,3 +22,19 @@ var animatePoints = function() {
         
         revealTitleText();
     };
+
+window.onload = function() {
+
+    if (window.innerHeight > 700) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 5;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+}
