d2096ee14c62515e39ce81606e713005f1669cbc
Bob-e <bob.e.mercer@gmail.com>
2017-06-22 20:47:26 -0400

    Assignment 9 Dom Event Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,23 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoints = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function(points) {
+    forEach(points, revealPoints);    
+    var hero = document.getElementsByClassName('hero-title');
 
-
-        var revealPoints = function(index) {
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translateY(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-        var hero = document.getElementsByClassName('hero-title');
-
-        var revealTitleText = function() {
-            hero[0].style.transform = "scaleX(1) translateY(-50%)";
-            hero[0].style.msTransform = "scaleX(1) translateY(-50%)";
-            hero[0].style.WebkitTransform = "scaleX(1) translateY(-50%)";
-        }
-                for (var i = 0; i < points.length; i++) {
-            revealPoints(i);
-        }
-        
-        revealTitleText();
-    };
+    var revealTitleText = function() {
+        hero[0].style.transform = "scaleX(1) translateY(-50%)";
+        hero[0].style.msTransform = "scaleX(1) translateY(-50%)";
+        hero[0].style.WebkitTransform = "scaleX(1) translateY(-50%)";
+    }
+    revealTitleText();
+};
 
 window.onload = function() {
 
