4a3811c296a9be303ee1c1cc28d8913f794dfe1d
Bob-e <bob.e.mercer@gmail.com>
2017-06-24 15:56:15 -0400

    Assignment 11 DOM: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumLOL = {
+    title: 'LOL<(^^,)>',
+    artist: 'Basshunter',
+    label: 'Ultra Music',
+    year: '2006',
+    albumArtUrl: 'assets/images/album_covers/LOL.jpg',
+    songs: [
+        { title: 'Now You\'re Gone', duration: '2:39' },
+        { title: 'DOTA', duration: '3:21' },
+        { title: 'Boten Anna', duration: '3:27'},
+        { title: 'I\'m Your Bass Creator', duration: '5:24' },
+        { title: 'Russia Privjet', duration: '4:10'}
+    ]
+};
+
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -40,24 +55,21 @@ var createSongRow = function (songNumber, songName, songLength) {
     return template;
 }
 
-var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-    // #2
+var setCurrentAlbum = function(album) {
+
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
 
-    // #3
     albumSongList.innerHTML = '';
 
-    // #4
     for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
@@ -65,4 +77,16 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumLOL];
+    var index = 1;
+
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
 };
